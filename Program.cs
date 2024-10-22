fe99f40451a4708cd0278d49883fcb706a57c2d0
lodhe5589 <rlodhe5589@gmail.com>
2024-10-22 19:45:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,53 @@
-DECLARE @MyTableParam MyTableType;
-INSERT INTO @MyTableParam (GuidId)
-SELECT GuidId FROM @MyTableParam;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
-// Here we add our join query
-SELECT * 
-FROM @MyTableParam TP
-INNER JOIN ViewTable VT ON TP.GuidId = VT.GuidId
+namespace TableTypeInLine
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string connectionString = "ConnectString";
 
+
+                DataTable myTable = new DataTable();
+                myTable.Columns.Add("Id", typeof(int));
+
+
+                myTable.Rows.Add(1);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sqlQuery = @"SELECT * FROM @MyTableParam;";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                     
+                        SqlParameter tvpParam = command.Parameters.AddWithValue("@MyTableParam", myTable);
+                        tvpParam.SqlDbType = SqlDbType.Structured;
+                        tvpParam.TypeName = "MyTableType";
+
+                        
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"Id: {reader["Id"]}");
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
