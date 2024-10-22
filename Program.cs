7485f2e48435d27c18c19540859ae5d347da1ee5
lodhe5589 <rlodhe5589@gmail.com>
2024-10-22 18:40:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
+DECLARE @MyTableParam MyTableType;
+INSERT INTO @MyTableParam (GuidId)
+SELECT GuidId FROM @MyTableParam;
+
+// Here we add our join query
+SELECT * 
+FROM @MyTableParam TP
+INNER JOIN ViewTable VT ON TP.GuidId = VT.GuidId
 
